d89d8e741f5591d51a2d7cc39e36736d069ecb9a
Weslley Alberto <weslleyalberto@gmail.com>
2021-12-18 14:10:49 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculadora2.Model
+{
+    public class Calculadora
+    {
+        public static void Somar(int x, int y)
+        {
+            Console.WriteLine($"A soma de {x} e {y} é = {x+y}");
+        }
+        public static void Subritair(int x, int y)
+        {
+            Console.WriteLine($"A subritação de {x} e {y} =  {x - y}");
+
+        }
+        public static void Divisao(int x, int y)
+        {
+            Console.WriteLine($"A divisão de {x} e { y} = { x / y}");
+        }
+        public static void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"A multiplicação de  {x} e { y} = {x * y}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Calculadora2.Model;
+using System;
+
+namespace Calculadora2
+{
+    internal class Program
+    {
+        public delegate void Operacao(int x, int y);
+        static void Main(string[] args)
+        {
+            Operacao op = new(Calculadora.Somar);
+            Operacao op1 = Calculadora.Subritair;
+            // Ambas manueira de invocar delegates
+            op.Invoke(1, 2); 
+            op1(1, 2);
+            Console.WriteLine(op);
+            Console.WriteLine(op1);
+        }
+    }
+}
